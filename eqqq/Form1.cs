f2233bea8e76be61a745180eee9fcc92233608e6
rrrinnn <46429539+plkkz@users.noreply.github.com>
2023-11-18 12:34:53 +0300

    Exception

--- eqqq/Form1.cs
+++ eqqq/Form1.cs
@@ -26,8 +26,16 @@ namespace eqqq
         {
             textBox5.Clear();
             int x = Convert.ToInt32(textBox1.Text);
+            try
+            {
             double y = (2 * x - 7) / Math.Sqrt(3 * x + 21);
             textBox5.Text = y.ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Processing failed: {ex.Message}");
+            }
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -51,28 +59,49 @@ namespace eqqq
             textBox9.Clear();
             textBox10.Clear();
             int x = Convert.ToInt32(textBox4.Text);
+            try
+            {
             double y1 = (2 * x - 7) / Math.Sqrt(3 * x + 21);
             textBox8.Text = y1.ToString();
             double y2 = Math.Sqrt(x / (x - 1));
             textBox9.Text = y2.ToString();
             double y3 = ((x + 1) / (x - 1)) + Math.Sqrt(Math.Pow(x, 2));
             textBox10.Text = y3.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Processing failed: {ex.Message}");
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             textBox6.Clear();
             int x = Convert.ToInt32(textBox2.Text);
+            try
+            {
             double y = Math.Sqrt(x/(x-1));
             textBox6.Text = y.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Processing failed: {ex.Message}");
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             textBox7.Clear();
             int x = Convert.ToInt32(textBox3.Text);
+            try
+            {
             double y = ((x+1)/(x-1))+Math.Sqrt(Math.Pow(x,2));
             textBox7.Text = y.ToString();   
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Processing failed: {ex.Message}");
+            }
         }
     }
 }
